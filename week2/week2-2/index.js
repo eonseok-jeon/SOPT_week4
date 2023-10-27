ae0144f8f479a63d30a6ee0853d8337b90a05da1
Jeon Eonseok <jseokeon@gmail.com>
2023-10-27 19:37:52 +0900

    feat: 수입 지출 버튼 기능 구현

--- week2/week2-2/index.js
+++ week2/week2-2/index.js
@@ -32,48 +32,80 @@ let totalBalance = INIT_BALANCE; // 굳이 왜 이렇게?
 let totalIncome = 0;
 let totalSpending = 0;
 
-HISTORY_LIST.forEach(({ category, location, price, type }) => {
-  const EACH_HISTORY = document.createElement('li');
-  EACH_HISTORY.classList.add('detail__list__item');
-  TOTAL_HISTORY.appendChild(EACH_HISTORY);
-  
-  const HISTORY_CATEGORY = document.createElement('p');
-  HISTORY_CATEGORY.classList.add('detail__list__item__category');
-  HISTORY_CATEGORY.textContent = category;
-  EACH_HISTORY.appendChild(HISTORY_CATEGORY);
-  
-  const HISTORY_DETAIL = document.createElement('div');
-  HISTORY_DETAIL.classList.add('detail__list__item__history');
-  EACH_HISTORY.appendChild(HISTORY_DETAIL);
-
-  const HISTORY_LOCATION = document.createElement('p');
-  HISTORY_LOCATION.classList.add('detail__list__item__history__content');
-  HISTORY_LOCATION.textContent = location;
-  HISTORY_DETAIL.appendChild(HISTORY_LOCATION);
-
-  const HISTORY_PRICE = document.createElement('p');
-  HISTORY_PRICE.classList.add('detail__list__item__history__price');
-  HISTORY_DETAIL.appendChild(HISTORY_PRICE);
-
-  if (type === '수입') {
-    totalBalance += price;
-    totalIncome += price;
-    HISTORY_PRICE.textContent = price.toLocaleString('ko-KR');
-    
-    return HISTORY_PRICE.classList.add('income');
-  }
-  if (type === '지출') {
-    totalBalance -= price;
-    totalSpending += price;
-    HISTORY_PRICE.textContent = (price * -1).toLocaleString('ko-KR');
-    
-    return HISTORY_PRICE.classList.add('spending');
-  }
-})
-
 const TOTAL_BALANCE = document.querySelector('.total__sum__amount');
-TOTAL_BALANCE.textContent = totalBalance.toLocaleString('ko-KR');
 const TOTAL_INCOME = document.querySelector('.total__detail__item__plus__num');
-TOTAL_INCOME.textContent = totalIncome.toLocaleString('ko-KR');
 const TOTAL_SPENDING = document.querySelector('.total__detail__item__minus__num');
-TOTAL_SPENDING.textContent = totalSpending.toLocaleString('ko-KR');
+
+const INCOME_CHECK_BOX = document.getElementById('income');
+const SPENDING_CHECK_BOX = document.getElementById('spending');
+// const TOTAL_HISTORY = document.querySelector('.detail__list');
+
+
+const updateHistory = function updateHistoryListWithCheckBox() {
+  const SHOW_INCOME = INCOME_CHECK_BOX.checked;
+  const SHOW_SPENDING = SPENDING_CHECK_BOX.checked;
+  
+  const EACH_HISTORY = document.querySelectorAll('.detail__list__item');
+  
+  EACH_HISTORY.forEach((item) => {
+    const type = item.querySelector('.detail__list__item__history__price').classList.contains('income')
+    ? '수입'
+    : '지출';
+    
+    if ((SHOW_INCOME && type === '수입') || (SHOW_SPENDING && type === '지출')) {
+      item.style.display = 'flex';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
+
+const main = () => {
+  INCOME_CHECK_BOX.addEventListener('change', updateHistory);
+  SPENDING_CHECK_BOX.addEventListener('change', updateHistory);
+  
+  HISTORY_LIST.forEach(({ category, location, price, type }) => {
+    const EACH_HISTORY = document.createElement('li');
+    EACH_HISTORY.classList.add('detail__list__item');
+    TOTAL_HISTORY.appendChild(EACH_HISTORY);
+    
+    const HISTORY_CATEGORY = document.createElement('p');
+    HISTORY_CATEGORY.classList.add('detail__list__item__category');
+    HISTORY_CATEGORY.textContent = category;
+    EACH_HISTORY.appendChild(HISTORY_CATEGORY);
+    
+    const HISTORY_DETAIL = document.createElement('div');
+    HISTORY_DETAIL.classList.add('detail__list__item__history');
+    EACH_HISTORY.appendChild(HISTORY_DETAIL);
+    
+    const HISTORY_LOCATION = document.createElement('p');
+    HISTORY_LOCATION.classList.add('detail__list__item__history__content');
+    HISTORY_LOCATION.textContent = location;
+    HISTORY_DETAIL.appendChild(HISTORY_LOCATION);
+    
+    const HISTORY_PRICE = document.createElement('p');
+    HISTORY_PRICE.classList.add('detail__list__item__history__price');
+    HISTORY_DETAIL.appendChild(HISTORY_PRICE);
+    
+    if (type === '수입') {
+      totalBalance += price;
+      totalIncome += price;
+      HISTORY_PRICE.textContent = price.toLocaleString('ko-KR');
+      
+      return HISTORY_PRICE.classList.add('income');
+    }
+    if (type === '지출') {
+      totalBalance -= price;
+      totalSpending += price;
+      HISTORY_PRICE.textContent = (price * -1).toLocaleString('ko-KR');
+      
+      return HISTORY_PRICE.classList.add('spending');
+    }
+  })
+
+  TOTAL_BALANCE.textContent = totalBalance.toLocaleString('ko-KR');
+  TOTAL_INCOME.textContent = totalIncome.toLocaleString('ko-KR');
+  TOTAL_SPENDING.textContent = totalSpending.toLocaleString('ko-KR');
+}
+
+main();
